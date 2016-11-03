2ef247f0577571da17b2ffd2b3779c8568c0c733
Federico Bond <federicobond@gmail.com>
2016-11-02 22:24:50 -0300

    Use async/await for tests

--- test/paypub.js
+++ test/paypub.js
@@ -11,219 +11,43 @@ contract('PayPub', function(accounts) {
     '0x95d64cacce0f0e5b0d1b843862f0accfadb787a4cabb8a88f7f1694ea232a5fc',
   ];
 
-  it("should store hashes in array", function(done) {
-    var pp;
+  it("should store hashes in array", async function() {
+    var pp = await PayPub.new(hashes);
 
-    PayPub.new(hashes)
-      .then(_pp => pp = _pp)
-      .then(() => pp.hashes(2))
-      .then(hash => assert.equal(hash, hashes[2]))
-      .then(done);
+    var hash = await pp.hashes(2);
+    assert.equal(hash, hashes[2]);
   });
 
-  it("should allow paying to a hash", function(done) {
-    var pp;
+  it("should allow paying to a hash", async function() {
+    var pp = await PayPub.new(hashes);
 
-    PayPub.new(hashes)
-      .then(_pp => pp = _pp)
-      .then(() => pp.pay(hashes[2], { value: 100 }))
-      .then(() => pp.chunks(hashes[2]))
-      .then(chunk => assert.equal(new Chunk(chunk).value, 100))
-      .then(done);
+    await pp.pay(hashes[2], { value: 100 });
+    var chunk = await pp.chunks(hashes[2]);
+
+    assert.equal(new Chunk(chunk).value, 100);
   });
 
-  it("should allow releasing a chunk password and claiming its value", function(done) {
-    var pp;
+  it("should allow releasing a chunk password and claiming its value", async function() {
 
     var password = 'foo';
     var hash = '0x2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae';
 
-    PayPub.new([hash])
-      .then(_pp => pp = _pp)
-      .then(() => pp.pay(hash, { from: accounts[0], value: 100 }))
-      .then(() => pp.release(password, { from: accounts[1] }))
-      .then(() => pp.payments(accounts[1]))
-      .then(received => assert.equal(received, 100))
-      .then(done);
+    var pp = await PayPub.new([hash]);
+
+    await pp.pay(hash, { from: accounts[0], value: 100 })
+    await pp.release(password, { from: accounts[1] })
+
+    var received = await pp.payments(accounts[1])
+    assert.equal(received, 100);
   })
 
-  it ("should use sha256 for computeHash", function(done) {
+  it ("should use sha256 for computeHash", async function() {
+
     var expected = '0x2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae';
 
-    PayPub.new()
-      .then(pp => pp.computeHash("foo"))
-      .then(hash => assert.equal(hash, expected))
-      .then(done);
+    var pp = await PayPub.new();
+    var hash = await pp.computeHash("foo");
+
+    assert.equal(hash, expected);
   })
 });
-
-  /*
-var sendReward = function(sender, receiver, value){
-  web3.eth.sendTransaction({
-    from:sender,
-    to:receiver,
-    value: value
-  })
-}
-
-contract('Bounty', function(accounts) {
-  it("creates bounty contract with factory address", function(done){
-    var target = SecureTargetMock.deployed();
-
-    Bounty.new(target.address).
-      then(function(bounty){
-        return bounty.factoryAddress.call()
-      }).
-      then(function(address){
-        assert.equal(address, target.address)
-      }).
-      then(done);
-  })
-
-  it("sets reward", function(done){
-    var target = SecureTargetMock.deployed();
-    var owner = accounts[0];
-    var reward = web3.toWei(1, "ether");
-
-    Bounty.new(target.address).
-      then(function(bounty){
-        sendReward(owner, bounty.address, reward);
-        assert.equal(reward, web3.eth.getBalance(bounty.address).toNumber())
-      }).
-      then(done);
-  })
-
-  it("cannot create bounty without address", function(done){
-    var target = SecureTargetMock.deployed();
-    Bounty.new().
-      then(function(bounty){
-        throw {name : "NoThrowError", message : "should not come here"};
-      }).
-      catch(function(error){
-        assert.notEqual(error.name, "NoThrowError");
-      }).
-      then(done);
-  })
-
-  it("empties itself when killed", function(done){
-    var target = SecureTargetMock.deployed();
-    var owner = accounts[0];
-    var reward = web3.toWei(1, "ether");
-    var bounty;
-    Bounty.new(target.address).
-      then(function(_bounty){
-        bounty = _bounty;
-        sendReward(owner, bounty.address, reward);
-        assert.equal(reward, web3.eth.getBalance(bounty.address).toNumber())
-        return bounty.kill()
-      }).
-      then(function(){
-        assert.equal(0, web3.eth.getBalance(bounty.address).toNumber())
-      }).
-      then(done);
-  })
-
-  describe("Against secure contract", function(){
-    it("checkInvariant returns true", function(done){
-      var targetFactory = SecureTargetFactory.deployed();
-      var bounty;
-      Bounty.new(targetFactory.address).
-        then(function(_bounty) {
-          bounty = _bounty;
-          return bounty.createTarget();
-        }).
-        then(function() {
-          return bounty.checkInvariant.call()
-        }).
-        then(function(result) {
-          assert.isTrue(result);
-        }).
-        then(done);
-    })
-
-    it("cannot claim reward", function(done){
-      var targetFactory = SecureTargetFactory.deployed();
-      var owner = accounts[0];
-      var researcher = accounts[1];
-      var reward = web3.toWei(1, "ether");
-
-      Bounty.new(targetFactory.address).
-        then(function(bounty) {
-          var event = bounty.TargetCreated({});
-          event.watch(function(err, result) {
-            event.stopWatching();
-            if (err) { throw err }
-            var targetAddress = result.args.createdAddress;
-            sendReward(owner, bounty.address, reward);
-            assert.equal(reward, web3.eth.getBalance(bounty.address).toNumber())
-            bounty.claim(targetAddress, {from:researcher}).
-              then(function(){ throw("should not come here")}).
-              catch(function() {
-                return bounty.claimed.call();
-              }).
-              then(function(result) {
-                assert.isFalse(result);
-                bounty.withdrawPayments({from:researcher}).
-                  then(function(){ throw("should not come here")}).
-                  catch(function() {
-                    assert.equal(reward, web3.eth.getBalance(bounty.address).toNumber())
-                    done();
-                  })
-              })
-          })
-          bounty.createTarget({from:researcher});
-        })
-    })
-  })
-
-  describe("Against broken contract", function(){
-    it("checkInvariant returns false", function(done){
-      var targetFactory = InsecureTargetFactory.deployed();
-      var bounty;
-      Bounty.new(targetFactory.address).
-        then(function(_bounty) {
-          bounty = _bounty;
-          return bounty.createTarget();
-        }).
-        then(function() {
-          return bounty.checkInvariant.call()
-        }).
-        then(function(result) {
-          assert.isFalse(result);
-        }).
-        then(done);
-    })
-
-    it("claims reward", function(done){
-      var targetFactory = InsecureTargetFactory.deployed();
-      var owner = accounts[0];
-      var researcher = accounts[1];
-      var reward = web3.toWei(1, "ether");
-
-      Bounty.new(targetFactory.address).
-        then(function(bounty) {
-          var event = bounty.TargetCreated({});
-          event.watch(function(err, result) {
-            event.stopWatching();
-            if (err) { throw err }
-            var targetAddress = result.args.createdAddress;
-            sendReward(owner, bounty.address, reward);
-            assert.equal(reward, web3.eth.getBalance(bounty.address).toNumber())
-            bounty.claim(targetAddress, {from:researcher}).
-              then(function() {
-                return bounty.claimed.call();
-              }).
-              then(function(result) {
-                assert.isTrue(result);
-                return bounty.withdrawPayments({from:researcher})
-              }).
-              then(function() {
-                assert.equal(0, web3.eth.getBalance(bounty.address).toNumber())
-              }).then(done);
-          })
-          bounty.createTarget({from:researcher});
-        })
-    })
-  })
-});
-*/
